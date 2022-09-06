a59bfdeaf71f994507330b4e9215a1972d216fbe
ViníciusYoda <rm92969@fiap.com.br>
2022-09-06 09:11:05 -0300

    A

--- rm92969/cp1/src/App.jsx
+++ rm92969/cp1/src/App.jsx
@@ -15,6 +15,10 @@ export default function App(){
             categoria : "higiene"
         },
         {
+            nomeProduto : "Desodorante",
+            qtd : 1230, 
+            categoria : "higiene"
+        },{
             nomeProduto : "Netrogena",
             qtd : 1230, 
             categoria : "higiene"
@@ -22,14 +26,23 @@ export default function App(){
             nomeProduto : "Netrogena",
             qtd : 1230, 
             categoria : "higiene"
-        },
+        },{
+            nomeProduto : "Netrogena",
+            qtd : 1230, 
+            categoria : "higiene"
+        },{
+            nomeProduto : "Netrogena",
+            qtd : 1230, 
+            categoria : "higiene"
+        }
     ]
 
-    const[produtos, setProdutos] = useState()
+    const[produtos, setProdutos] = useState(listaDeProdutos)
 
     return(
         <>
             <h1>CP1 de RWD 2º SEMESTRE</h1>
+            <Produtos produtos={produtos} />
         </>
     )
 }
