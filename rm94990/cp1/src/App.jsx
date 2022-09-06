b3fe64d8658cd7eee9bb38667ff426ce3fe24a20
augustopdro <rm94990@fiap.com.br>
2022-09-06 08:55:38 -0300

    useState adicionado e carregado com lista de obj.

--- rm94990/cp1/src/App.jsx
+++ rm94990/cp1/src/App.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 export default function App() {
+  const listaDeProdutos = [
+    {
+      nomeProduto: "Mouse Óptico",
+      qtd: 100,
+      categoria: "informatica",
+    },
+    {
+      nomeProduto: "Metanfetamina",
+      qtd: 10,
+      categoria: "Saúde",
+    },
+    {
+      nomeProduto: "BMW",
+      qtd: 80,
+      categoria: "Veículos",
+    },
+    {
+      nomeProduto: "Kalashnikov",
+      qtd: 27,
+      categoria: "Armas",
+    },
+    {
+      nomeProduto: "Biblia",
+      qtd: 48,
+      categoria: "Religião",
+    },
+  ];
+
+  const [produtos, setProdutos] = useState(listaDeProdutos);
+
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
+    <>
+      <h1>CP1 de RWD 2º SEMESTRE</h1>
+    </>
   );
 }
