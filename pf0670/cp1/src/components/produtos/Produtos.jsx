183e3e6615e40e24a1720e0ca31dc272d2b7da20
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-06 09:55:35 -0300

    Estilizando a tabela da lista de obj.

--- pf0670/cp1/src/components/produtos/Produtos.jsx
+++ pf0670/cp1/src/components/produtos/Produtos.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './Produtos.css'
 export default function Produtos(props) {
     return (
         <div>
             <h2>PRODUTOS</h2>
 
-            <table border={1}>
+            <table className="tblProdutos" border={1}>
                 <thead>
                     <tr>
                         <th>ID</th>
@@ -17,7 +17,8 @@ export default function Produtos(props) {
                 <tbody>
 
                     {props.produtos.map((p, i) =>
-                        <tr key={i} style={i % 2 === 0 ? { backgroundColor: "#ccc" } : { backgroundColor: "#afc" }}>
+                        // <tr key={i} style={i % 2 === 0 ? { backgroundColor: "#ccc" } : { backgroundColor: "#afc" }}>
+                        <tr key={i}>
                             <td>{i + 1}</td>
                             <td>{p.nomeProduto}</td>
                             <td>{p.qtd}</td>
