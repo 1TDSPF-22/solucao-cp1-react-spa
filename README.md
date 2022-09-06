12ffcd72f67684aab2c18b8a3f238b4d49d0153f
btwbeatriz <rm95246@fiap.com.br>
2022-09-06 09:30:14 -0300

    Utilizando lista com map

--- README.md
+++
@@ -1,2 +0,0 @@
-# solucao-cp1-react-spa
-Solução do CP1 de React SPA
--- rm95246/cp1/src/App.jsx
+++ rm95246/cp1/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { useState } from "react"
+import Produtos from "./components/produtos/Produtos"
+
 
 export default function App() {
 
@@ -37,33 +39,39 @@ export default function App() {
         {
             nomeProduto: "Mouse Óptico",
             qtd: 100,
-            categoria: "informatica"
+            categoria: "Informatica"
         },
         {
-            nomeProduto: "Netrogena",
+            nomeProduto: "Neutrogena",
             qtd: 1230,
-            categoria: "higiene"
+            categoria: "Higiene"
         },
         {
             nomeProduto: "Desodorante Avanço",
             qtd: 5000,
-            categoria: "perfumaria"
+            categoria: "Perfumaria"
         },
         {
             nomeProduto: "Balde de PVC",
             qtd: 555,
-            categoria: "bazar"
+            categoria: "Bazar"
         },
         {
-            nomeProduto: "Acem sem Osso",
+            nomeProduto: "Acém sem Osso",
             qtd: 10000,
-            categoria: "acougue"
+            categoria: "Açougue"
+        },
+        {
+            nomeProduto: "Toca de lã",
+            qtd: 55,
+            categoria: "Textil"
         }
     ])
 
     return (
         <>
             <h1>CP1 de RWD 2º SEMESTRE</h1>
+            <Produtos produtos={produtos}/>
         </>
     )
 }
