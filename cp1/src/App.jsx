b167e47723565f2f97c88e92fe16d2d8eac46f9d
averlanmarques <rm95314@fiap.com.br>
2022-09-06 08:56:04 -0300

    useState declarando lista de produtos

---
+++ cp1/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import { useState } from "react"
+
+
+export default function App() {
+
+    const listaDeProdutos = [
+        {
+            nomeProduto: "Mouse Óptico",
+            qtd: 100,
+            categoria: "informática"
+        },
+        {
+            nomeProduto: "Teclado Mecânico",
+            qtd: 100,
+            categoria: "informática"
+        }, 
+        {
+            nomeProduto: "Pc Gamer",
+            qtd: 100,
+            categoria: "informática"
+        }, 
+        {
+            nomeProduto: "HD",
+            qtd: 50,
+            categoria: "informática"
+        }, 
+        {
+            nomeProduto: "SSD",
+            qtd: 50,
+            categoria: "informática"
+        }, 
+    ]
+
+    cost[produtos, setProdutos] = useState()    
+
+    return(
+        <div>
+            <h1>CP1 de RWM 2² SEMESTRE</h1>
+        </div>
+    )
+
+}
