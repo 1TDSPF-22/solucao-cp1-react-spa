d5e278d39f9e0ea4b4286fdfccd5012bd697e768
phalves23 <rm93693@fiap.com.br>
2022-09-06 09:30:46 -0300

    :sparkles: consumindo lista com map e trazendo as informações para uma tabela

--- rm93693/cp1/src/produtos/Produtos.jsx
+++ rm93693/cp1/src/produtos/Produtos.jsx
@@ -4,7 +4,33 @@ export default function Produtos(props) {
   return (
     <>
       <h2>Produtos</h2>
-      <p>{props.produtos[0].nomeProduto}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.produtos.map((produto, id) => (
+            <tr key={id}>
+              <td>{id + 1}</td>
+              <td>{produto.nomeProduto}</td>
+              <td>{produto.qtd}</td>
+              <td>{produto.categoria}</td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={4}>
+              <caption>PRODUTOS</caption>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
     </>
   );
 }
