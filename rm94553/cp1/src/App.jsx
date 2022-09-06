468db8deca3a52a8c1511b4b4b5481973bdb14ee
igorcamargo <rm94553@fiap.com.br>
2022-09-06 08:55:59 -0300

    useState carregando e criando uma lista de obj

--- rm94553/cp1/src/App.jsx
+++ rm94553/cp1/src/App.jsx
@@ -1,10 +1,70 @@
 import React from "react"
+import { useState } from "react"
 
 
 export default function App(){
+    
+    // const listaDeProdutos = [
+    //     {
+    //         nomeProduto: "mouse",
+    //         qtd: 100,
+    //         categoria: "informática"
+    //     },
+    //     {
+    //         nomeProduto: "lápis",
+    //         qtd: 50,
+    //         categoria: "material-escolar"
+    //     },
+    //     {
+    //         nomeProduto: "camisa",
+    //         qtd: 12,
+    //         categoria: "roupas"
+    //     },
+    //     {
+    //         nomeProduto: "PS4",
+    //         qtd: 150,
+    //         categoria: "informática"
+    //     },
+    //     {
+    //         nomeProduto: "celular",
+    //         qtd: 180,
+    //         categoria: "informática"
+    //     },
+    // ]
+    
+    // const[produtos, setProdutos] = useState(listaDeProdutos)
+    const[produtos, setProdutos] = useState = ([
+        {
+            nomeProduto: "mouse",
+            qtd: 100,
+            categoria: "informática"
+        },
+        {
+            nomeProduto: "lápis",
+            qtd: 50,
+            categoria: "material-escolar"
+        },
+        {
+            nomeProduto: "camisa",
+            qtd: 12,
+            categoria: "roupas"
+        },
+        {
+            nomeProduto: "PS4",
+            qtd: 150,
+            categoria: "informática"
+        },
+        {
+            nomeProduto: "celular",
+            qtd: 180,
+            categoria: "informática"
+        },
+    ])
+    
+    
     return(
-        <div>
-            <h1>APP</h1>
-        </div>
+        <>
+            <h1>CP! de RWWD 2º SEMESTRE</h1>
+        </>
     )
 }
