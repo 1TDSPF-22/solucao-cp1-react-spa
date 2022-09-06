080bd44a9e2ab9544152417d6f11261a9fbe7102
phalves23 <rm93693@fiap.com.br>
2022-09-06 08:55:53 -0300

    :sparkles: useState declarado e carregado com lista de produtos

---
+++ rm93693/cp1/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+
+// const listaDeProdutos = [
+//   { nomeProduto: "Mouse Óptico", qtd: 100, categoria: "Informática" },
+//   { nomeProduto: "Fone", qtd: 40, categoria: "Informática" },
+//   { nomeProduto: "Celular", qtd: 200, categoria: "Informática" },
+//   { nomeProduto: "Teclado", qtd: 100, categoria: "Informática" },
+//   { nomeProduto: "Monitor", qtd: 100, categoria: "Informática" },
+// ];
+
+export default function App() {
+  const [produtos, setProdutos] = useState([
+    { nomeProduto: "Mouse Óptico", qtd: 100, categoria: "Informática" },
+    { nomeProduto: "Fone", qtd: 40, categoria: "Informática" },
+    { nomeProduto: "Celular", qtd: 200, categoria: "Informática" },
+    { nomeProduto: "Teclado", qtd: 100, categoria: "Informática" },
+    { nomeProduto: "Monitor", qtd: 100, categoria: "Informática" },
+  ]);
+
+  return (
+    <>
+      <h1>CP1 de RWD 2º SEMESTRE</h1>
+    </>
+  );
+}
