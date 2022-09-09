1c7436252834429811b5aeaa31204664aa77696d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-09 12:17:12 -0300

    Sass basics

--- pf0670/cp1/src/App.jsx
+++ pf0670/cp1/src/App.jsx
@@ -71,6 +71,14 @@ export default function App() {
     return (
         <>
             <h1>CP1 de RWD 2º SEMESTRE</h1>
+
+            <div>
+                Escopo da DIV
+                <p>
+                    Escopo do P
+                </p>
+            </div>
+
             <Produtos produtos={produtos} />
         </>
     )
